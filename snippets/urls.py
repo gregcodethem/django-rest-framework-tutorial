2d255a0f363ee118f19ba51e1e402162a547725f
Greg Shepley <gregshepley@yahoo.co.uk>
2018-12-02 17:07:56 +0000

    viewset classes, routers and schema added using core api

--- snippets/urls.py
+++ snippets/urls.py
@@ -15,22 +15,18 @@ Including another URLconf
 from django.urls import path
 from snippets import views
 """
-from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
 from snippets import views
 
-urlpatterns = [
-    path('snippets/', views.SnippetList.as_view(),
-         name='snippet-list'),
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view(),
-         name='snippet-detail'),
-    path('users/', views.UserList.as_view(), name='user-list'),
-    path('users/<int:pk>/', views.UserDetail.as_view(),
-         name='user-detail'),
-    path('', views.api_root),
-    path('snippets/<int:pk>/highlight/',
-         views.SnippetHighlight.as_view(),
-         name='snippet-highlight'),
-]
+schema_view = get_schema_view(title="Pastebin API")
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
+
+urlpatterns = [
+    path('schema/', schema_view),
+    path('', include(router.urls)),
+]
